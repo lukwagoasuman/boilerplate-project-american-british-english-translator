f11772bc2dc0fef5b151ddf18c0a473be0f50837
lukwagoasuman <lukwagoasuman236@gmail.com>
2024-09-19 10:48:59 +0300

    Initial Commit

--- components/translator.js
+++ components/translator.js
@@ -5,6 +5,66 @@ const britishOnly = require('./british-only.js')
 
 class Translator {
 
+    americanToBritish(text) {
+        const wordDiffAndSpelling = {...americanOnly, ...americanToBritishSpelling }
+        const titles = americanToBritishTitles;
+
+        return this.translat(text, wordDiffAndSpelling, titles)
+    }
+
+    britishToAmerican(text) {
+                        const britishToAmericanSpelling = this.objectFlip(americanToBritishSpelling)
+                        const wordDiffAndSpelling = {...britishOnly, ...britishToAmericanSpelling }
+                        const britishToAmericanTitles = this.objectFlip(americanToBritishTitles);
+            
+                        return this.translat(text, wordDiffAndSpelling, britishToAmericanTitles);
+            
+                    }
+
+    translat(text, wordDiffAndSpelling, titles) {
+        const textLowerCase = text.toLowerCase();
+        let translated;
+        const timeRegex = /(([0-9]|0[0-9]|1[0-9]|2[0-3])(:|\.)([0-5][0-9]))/g;
+
+        // searching for  Titles and replace them
+        Object.entries(titles) //[ [key1, value1],[key1, value1] ]
+            .map(([key, value]) => {
+                if (textLowerCase.includes(key)) {
+                    translated = text.replace(new RegExp(key, "gi"), `<span class="highlight">${this.capitalizeFirstLetter(value)}</span>`) || text;
+                }
+            })
+
+        translated = translated || text; //make sure that translated not null || undefiend
+
+
+        // change time format  12:15 <=> 12.15
+        const changeTime = textLowerCase.match(timeRegex); //['12.15','11.15']
+        if (changeTime) {
+            changeTime.map(time => {
+                translated = translated.replace(time, `<span class="highlight">${time.replace(':', '.')}</span>`) || text;
+            })
+        }
+
+        Object.entries(wordDiffAndSpelling)
+            .map(([key, value]) => {
+                if (new RegExp(`${key} `, "gi").test(textLowerCase) ||
+                    new RegExp(`${key}[^A-Za-z]`, "gi").test(textLowerCase) ||
+                    new RegExp(`${key}$`, "gi").test(textLowerCase)) {
+
+                    translated = translated.replace(new RegExp(key, "gi"), `<span class="highlight">${value}</span>`) || text;
+                }
+            });
+
+        return translated || text;
+    }
+
+    objectFlip(obj) {
+        return Object.entries(obj).reduce((acc, [key, value]) => (acc[value] = key, acc), {})
+    }
+
+    capitalizeFirstLetter(word) {
+        return word[0].toUpperCase() + word.slice(1);
+    }
 }
 
 module.exports = Translator;
